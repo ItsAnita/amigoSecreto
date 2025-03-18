9a042aa55edb4b3b1b93896e25e8542d62a34c83
ItsAnita <aant25220@gmail.com>
2025-03-17 19:43:35 -0600

    creacion de funcion para agregar amigos al array

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,26 @@
 // Array para almacenar los nombres de los amigos
 let amigos = [];
+
+// Funcion para agregar un amigo a la lista
+function agregarAmigo(){
+    //Capturar el valor del campo de entrada
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value.trim();
+    
+    // Validar la entrada
+    if (nombreAmigo === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    
+    // Actualizar el array de amigos
+    amigos.push(nombreAmigo);
+    console.log('Nombres: ', amigos);
+    
+    // Limpiar el campo de entrada
+    inputAmigo.value = '';
+    
+    // Enfocar nuevamente el campo para facilitar añadir más amigos
+    inputAmigo.focus();
+    
+}
